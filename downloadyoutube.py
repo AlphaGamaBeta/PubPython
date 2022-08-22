e5ddd1462b23231b6deaef88536613e96922ea60
Dysprosium <110243616+AlphaGamaBeta@users.noreply.github.com>
2022-08-23 01:52:44 +0300

    Update downloadyoutube.py

--- downloadyoutube.py
+++ downloadyoutube.py
@@ -1,15 +1,24 @@
 from pytube import YouTube
-#This version give you the ability to only download highest quality and mp4 extension only 
+#Simple one 
 
-def download_mp4_video(url: str):
-    yt = YouTube(url)
+def progress(stream,chunk,bytes_remaining):
+    print(f"Video is being download! remaining {bytes_remaining}")
+def complete(stream, file_path):
+    print(f"Downloading completed {file_path}")
+def download_1080p_mp4_videos(url: str):
+    yt = YouTube(url=url,on_progress_callback=progress,on_complete_callback=complete)
     y=yt.streams.filter(file_extension="mp4")
     y=y.get_highest_resolution()
-    y=y.download(output_path="./")
+    y.download(output_path="./")
 
-url=input("Input the url: ")
-try:
-    download_mp4_video(url=url)
-except:
-    raise Exception("Check the url")
+def fun():
+    url = input("Input the url (q for exit): ")
+    if url == "q":
+        return
+    try:
+        download_1080p_mp4_videos(url=url)
+    except:
+        print("Check the URL \n ")
+        fun()
+fun()
 
